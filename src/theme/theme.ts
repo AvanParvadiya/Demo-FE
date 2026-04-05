33165b636595e3809f6cb5c8a53a5f0858ca703f
AvanParvadiya <avanparvadiya737@gmail.com>
2026-04-05 11:52:54 +0530

    feat: initialize custom MUI theme configuration with palette, typography, and component overrides

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -4,7 +4,7 @@ const theme = createTheme({
   palette: {
     mode: "light",
     primary: {
-      main: "#1976d2",
+      main: "#2563eb",
       light: "#42a5f5",
       dark: "#1565c0",
       contrastText: "#ffffff",
@@ -16,7 +16,7 @@ const theme = createTheme({
       contrastText: "#ffffff",
     },
     background: {
-      default: "#e5f6fd",
+      default: "#f1f7fe",
       paper: "#ffffff",
     },
     text: {
